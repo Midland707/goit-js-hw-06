8fd7a83d1a8eed8eca3f6fccd296c68f5f9c3587
Midland707 <rus14job@ukr.net>
2022-12-04 20:04:36 +0200

    add task-07

--- js/task-07.js
+++ js/task-07.js
@@ -5,3 +5,24 @@
 // <input id="font-size-control" type="range" min="16" max="96" />
 // <br />
 // <span id="text">Abracadabra!</span>
+
+const inputRange = document.querySelector("#font-size-control");
+const text = document.querySelector("#text");
+
+let size = inputRange.value;
+const styles = document.querySelector("body");
+const stylesInsert = document.createElement("style");
+const textStyles = `#text {
+    font-size: ${size}px;
+  }`;
+stylesInsert.insertAdjacentHTML("beforeend", textStyles);
+styles.prepend(stylesInsert);
+
+inputRange.addEventListener("input", () => {
+  console.log(inputRange.value);
+  size = inputRange.value;
+  const textStyle = `#text {
+    font-size: ${size}px;
+  }`;
+  stylesInsert.innerHTML = textStyle;
+});
